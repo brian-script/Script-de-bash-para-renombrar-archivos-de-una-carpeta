565343bca13b9bd1b653a85128453c1bb0b58f25
brian <brianarguello358@gmail.com>
2024-11-08 23:17:28 -0300

    agregando comentarios

--- nombrarArchivos.sh
+++ nombrarArchivos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#preguntamos al usario que necesita
 echo -n "Agrega un directorio: "
 read Directorio
 
@@ -9,14 +10,18 @@ read extencion
 echo -n "El nombre que quieres que lleve los archivos: "
 read nombre
 
+#creamos un contador para diferenciar los archivos
 contador=1
 
+#creamos un ciclo for donde ordenamos los archivos de mas viejo a mas nuevo del directorio anteriormente pedido
 for archivo in $( ls -1tr "$Directorio" ); do
+    #verificamos que si es un archivo o una carpeta
     if [ -f "$Directorio/$archivo" ]; then
         mv "$Directorio/$archivo" "$Directorio/$nombre$contador.$extencion"
         contador=$((contador + 1))
     fi
 done 
 
+#mensaje en pantalla
 echo "Proceso terminado"
-echo "Verifica que de que haya funcion$extencion"
+echo "Verifica que de que haya funcion"
